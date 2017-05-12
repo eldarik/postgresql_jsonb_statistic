a6d464f03b930098529de360e313a4ff6b08e370
Eldar Iskhakov <iskhakoveldar@yandex.ru>
2017-05-12 19:15:27 +0300

    change jsonb_statistic--1.0.sql to files in src/sql path

--- jsonb_statistic/src/sql/extract_key_paths.sql
+++ jsonb_statistic/src/sql/extract_key_paths.sql
@@ -1,8 +1,3 @@
-CREATE table temporary_extract_key_paths(
-  key_path text,
-  value text
-);
-
 CREATE FUNCTION
   extract_key_paths(
     document jsonb,
@@ -33,16 +28,3 @@ BEGIN
   END IF;
 END;
 $$ LANGUAGE plpgsql;
-
---SELECT jsonb_extract_key_paths('{"IP":"10.0.0.3","nested": { "Roles":"master" } }');
---returns {'->IP','->nested->Roles'}
-CREATE FUNCTION
-  jsonb_extract_key_paths(document jsonb)
-  RETURNS text[] AS $$
-BEGIN
-  PERFORM extract_key_paths(document, '{}'::text[], '', '');
-  RETURN array(SELECT key_path from temporary_extract_key_paths);
-END;
-$$ LANGUAGE plpgsql;
-
-
---
+++ jsonb_statistic/src/sql/temporary_extract_key_paths.sql
@@ -0,0 +1,6 @@
+CREATE table temporary_extract_key_paths(
+  key_path text,
+  value text
+);
+
+
